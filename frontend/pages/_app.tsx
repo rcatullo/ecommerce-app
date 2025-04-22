44efb76ff73052c74e27d505ef3ba7db1edf3c31
Ryan Catullo <rcatullo@stanford.edu>
2025-04-22 12:31:04 -0700

    fix: remove ProductProvider from MyApp component; streamline context providers

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,11 @@
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
-import { ProductProvider } from '../context/ProductContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-        <ProductProvider>
-          <Component {...pageProps} />
-        </ProductProvider>
+      <Component {...pageProps} />
     </AuthProvider>
 
   );
