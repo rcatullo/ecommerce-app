01402c2de9aec61e5d006ac4c418cb2fd0f53b5a
Ryan Catullo <rcatullo@stanford.edu>
2025-04-22 01:44:39 -0700

    fix: remove CartProvider from MyApp component; streamline context providers

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,17 +1,14 @@
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/AuthContext';
-import { CartProvider } from '../context/CartContext';
 import { ProductProvider } from '../context/ProductContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <CartProvider>
         <ProductProvider>
           <Component {...pageProps} />
         </ProductProvider>
-      </CartProvider>
     </AuthProvider>
 
   );
